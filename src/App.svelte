94728d3f9f233f2077c55a57fd054504fb14509e
reductist <52290715+reductist@users.noreply.github.com>
2019-11-13 13:50:25 -0500

    fix filtering

--- src/App.svelte
+++ src/App.svelte
@@ -1,131 +1,145 @@
 <script>
-	import Table from './Table.svelte'
-	import Header from './Header.svelte'
-	import Rows from './Rows.svelte'
-	import Pagination from './Pagination.svelte'
-	import { onMount } from 'svelte';
+  import Table from './Table.svelte'
+  import Header from './Header.svelte'
+  import Rows from './Rows.svelte'
+  import Pagination from './Pagination.svelte'
+  import { onMount } from 'svelte';
 
-  export let url = 'https://jsonplaceholder.typicode.com/todos/';
-  export let data = [];
+  const url = 'https://jsonplaceholder.typicode.com/todos/';
+  let data = [];
 
-	onMount(async function() {
+  onMount(async function() {
     const response = await fetch(url);
     const json = await response.json();
     data = json;
   });
-	
-	let filters = {
-		item: {name: '', keys: ['name']}
-	}
-	let pageSize = 10;
-	let totalPages = 0;
-	let currentPage = 1;
-	function totalPagesChanged (e) {
-		totalPages = e.detail
-	}
-
-	// fetch endpoint for <Fetch /> component prop
-	const props = {
-    url: "https://jsonplaceholder.typicode.com/todos/1",
-  };
+  
+  let filters = {
+    title: {value: '', keys: ['title']}
+  }
+  
+  let pageSize = 10;
+  let totalPages = 0;
+  let currentPage = 1;
+  
+  function totalPagesChanged (e) {
+    totalPages = e.detail;
+  }
 </script>
 
 <style>
-	:root {
+  :root {
     background:#3d3f46;
     margin: 0 auto;
     overflow-x: hidden;
     width: 95%;
-	}
-	#wrapper {
-		font-family: 'Overpass Mono', sans-serif;
-		font-size: 14px;
-	}
-	#table-wrapper {
-		width: 100%;
-	}
-	td {
-		border: 1px solid #333;
-		border-collapse: collapse;
-		padding-left: 10px;
-		padding-top: 5px;
+  }
+  #wrapper {
+    font-family: 'Overpass Mono', sans-serif;
+    font-size: 14px;
+    margin-top: 2vh;
+    height: fit-content;
+  }
+  #table-wrapper {
+    width: 100%;
+  }
+  td {
+    font-family: 'Fira Code';
+    font-size: 12px;
+    color: #001122;
+    border: 0.5px solid #7e8e9f;
+    border-collapse: collapse;
+    padding-left: 10px;
+    padding-top: 5px;
     padding-bottom: 5px;
-		height: 30px;
-		font-size: 12px;
-		line-height: 0.7rem;
-	}
-	td:nth-child(1) {
-		text-align: center;
-	}
-	#filterInput {
+    height: 30px;
+    font-weight: 100;
+    line-height: 1rem;
+  }
+  td:nth-child(1) {
+    text-align: center;
+  }
+  td:hover {
+    background: #66686a !important;
+    color: #00ffc4 !important;
+    border: 1px solid #00ffc4 !important;
+    border-collapse: collapse !important;
+    font-weight: 400 !important;
+  }
+  #filterInput {
     color: #00ffc4;
     border: 1px solid #00ffc4;
     border-top-left-radius: 10px;
     background: #2d2f3a;
     padding-top: 8px;
+    padding-bottom: 8px;
     text-indent: 8px;
     transition: all 350ms;
     transition-timing-function: ease-out;
     margin-bottom: 0;
-	}
-	#filterInput:focus	{
-		border-radius: 2em;
-		border: 2px solid #00d5ff;
-		background: #fff;
-		color: #052223;
-		outline: none;
-		margin-bottom: 1em;
-	}
-	#filterInput:not(:placeholder-shown) {
-		border-radius: 2em;
-		border: 2px solid #00d5ff;
-		background: #fff;
-		color: #052223;
-		outline: none;
-		margin-bottom: 1em;
-	}
+  }
+  #filterInput:focus	{
+    border-radius: 2em;
+    border: 2px solid #00d5ff;
+    background: #fff;
+    color: #052223;
+    outline: none;
+    margin-bottom: 1em;
+  }
+  #filterInput:not(:placeholder-shown) {
+    border-radius: 2em;
+    border: 2px solid #00d5ff;
+    background: #fff;
+    color: #052223;
+    outline: none;
+    margin-bottom: 1em;
+  }
 </style>
 
 <div id='wrapper'>
 
-	<div id="filterDiv">
-		<input type="text" id="filterInput" placeholder="Filter" required bind:value="{filters.item.value}" />
-	</div>
+  <div id="filterDiv">
+    <input type="text" 
+           id="filterInput" 
+           placeholder="Filter"
+           required 
+           bind:value="{filters.title.value}"/>
+  </div>
 
-	<div id="table-wrapper">
-		<Table {data} {filters} {pageSize} {currentPage}
-						class="table"
-						selectedClass="table-info"
-						on:totalPagesChanged="{totalPagesChanged}"
-		>
+  <div id="table-wrapper">
+    <Table {data} {filters} {pageSize} {currentPage}
+            class="table"
+            selectedClass="table-info"
+            on:totalPagesChanged="{totalPagesChanged}"
+    >
 
-			<thead slot="head">
-				<Header sortKey="name" defaultSort="asc">Item</Header>
-				<Header sortKey='rarity'>Rarity</Header>
-				<Header sortKey='type'>Type</Header>
-			</thead>
+      <thead slot="head" let:displayData="{data}">
+        <Header sortKey="userId">User ID</Header>
+        <Header sortKey='title' defaultSort="asc">Title</Header>
+        <Header sortKey='completed'>Status</Header>
+      </thead>
 
-			<tbody slot="body" let:displayData="{displayData}">
-				{#each displayData as row (row.userId)}
-					<Rows {row}>
-						<td>{row.userId}</td>
-						{#if row.title === undefined}
-							<td>-</td>
-						{:else}	
-							<td>{row.title}</td>
-						{/if}
-						{#if row.completed === undefined}
-							<td>-</td>
-						{:else}
-							<td>{row.completed}</td>
-						{/if}
-					</Rows>
-				{/each}
-			</tbody>
-		</Table>
+      <tbody slot="body" let:displayData="{displayData}">
+        {#each displayData as row (row.title)}
+          <Rows {row}>
+            <td>{row.userId}</td>
+            {#if row.title === undefined}
+              <td>-</td>
+            {:else}	
+              <td>{row.title}</td>
+            {/if}
+            {#if row.completed === undefined}
+              <td>-</td>
+            {:else}
+              <td>{(row.completed) ? 'Complete' : 'Incomplete'}</td>
+            {/if}
+          </Rows>
+        {/each}
+      </tbody>
+    </Table>
 
-		<Pagination {totalPages} bind:currentPage="{currentPage}">
-		</Pagination>
+    <Pagination {totalPages} bind:currentPage="{currentPage}">
+    </Pagination>
 
-	</div>
+  </div>
 </div>
--- src/Pagination.svelte
+++ src/Pagination.svelte
@@ -9,7 +9,7 @@
   export let lastText = '->';
   export let directionLinks = true;
   // --- \\
-  
+
   $: displayPages = (() => {
     function displayAllPages() {
       const displayPages = []
@@ -162,51 +162,51 @@
 
 {#if !(hideSinglePage && totalPages === 1)}
 <nav class="smart-pagination">
-	<div class="pagination">
-		{#if boundaryLinks}
-		<span class:disabled="{currentPage === 1}" id="first-page" class="page-item boundary-link">
-			<a href="#javascript:void(0)" aria-label="Previous" on:click="{firstPage}" class="page-link">
-				<span aria-hidden="true">{@html firstText}</span>
-			</a>
-		</span>
-		{/if}
-		
-		{#if directionLinks }
-		<span class:disabled="{currentPage === 1}" class="page-item">
-			<a href="javascript:void(0)" aria-label="Previous" on:click="{previousPage}" class="page-link">
-				<slot name="previousIcon" disabled="{currentPage === 1}">
-					<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-						<path fill="currentColor"
-									d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path>
-					</svg>
-				</slot>
-			</a>
-		</span>
-		{/if}
-		{#each displayPages as page (page.value)}
-		<span class="page-item" class:active="{currentPage === page.value}">
-			<a href="#javascript:void(0)" on:click="{() => selectPage(page.value)}" class="page-link">{page.title}</a>
-		</span>
-		{/each}
-		{#if directionLinks}
-		<span class:disabled="{currentPage === totalPages}" class="page-item">
-			<a href="#javascript:void(0)" aria-label="Next" on:click="{nextPage}" class="page-link">
-				<slot name="nextIcon" disabled="{currentPage === totalPages}">
-					<svg width="16" height="16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-						<path fill="currentColor"
-									d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
-					</svg>
-				</slot>
-			</a>
-		</span>
-		{/if}
-		{#if boundaryLinks}
-			<span class:disabled="{currentPage === totalPages}" id="last-page" class="page-item boundary-link">
-				<a href="#javascript:void(0)" aria-label="Last" on:click="{lastPage}" class="page-link">
-					<span aria-hidden="true">{@html lastText}</span>
-				</a>
-		</span>
-		{/if}
-	</div>
+  <div class="pagination">
+    {#if boundaryLinks}
+    <span class:disabled="{currentPage === 1}" id="first-page" class="page-item boundary-link">
+      <a href="#javascript:void(0)" aria-label="Previous" on:click="{firstPage}" class="page-link">
+        <span aria-hidden="true">{@html firstText}</span>
+      </a>
+    </span>
+    {/if}
+
+    {#if directionLinks }
+    <span class:disabled="{currentPage === 1}" class="page-item">
+      <a href="javascript:void(0)" aria-label="Previous" on:click="{previousPage}" class="page-link">
+        <slot name="previousIcon" disabled="{currentPage === 1}">
+          <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+            <path fill="currentColor"
+                  d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path>
+          </svg>
+        </slot>
+      </a>
+    </span>
+    {/if}
+    {#each displayPages as page (page.value)}
+    <span class="page-item" class:active="{currentPage === page.value}">
+      <a href="#javascript:void(0)" on:click="{() => selectPage(page.value)}" class="page-link">{page.title}</a>
+    </span>
+    {/each}
+    {#if directionLinks}
+    <span class:disabled="{currentPage === totalPages}" class="page-item">
+      <a href="#javascript:void(0)" aria-label="Next" on:click="{nextPage}" class="page-link">
+        <slot name="nextIcon" disabled="{currentPage === totalPages}">
+          <svg width="16" height="16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+            <path fill="currentColor"
+                  d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
+          </svg>
+        </slot>
+      </a>
+    </span>
+    {/if}
+    {#if boundaryLinks}
+      <span class:disabled="{currentPage === totalPages}" id="last-page" class="page-item boundary-link">
+        <a href="#javascript:void(0)" aria-label="Last" on:click="{lastPage}" class="page-link">
+          <span aria-hidden="true">{@html lastText}</span>
+        </a>
+    </span>
+    {/if}
+  </div>
 </nav>
 {/if}
